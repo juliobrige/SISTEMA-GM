1e6876fef3dac1ff23dbdb6fd7ebeaa09b9036d9
Julio Benjamim Bernardo <juliobenjamimbernardo36@gmail.com>
2025-04-04 17:47:30 +0200

    new

--- mentorados/views.py
+++ mentorados/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from. models import Mentorados, Navigators
 
 def mentorados(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     if request.method == 'GET':
-        return render (request, 'mentorados.html')
+        navigators = Navigators.objects.filter(user=request.user)
+        return render (request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators})
