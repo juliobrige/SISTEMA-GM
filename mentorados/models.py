11e9f75f0c8c69d49671903394323d7384dbc1c1
Julio Benjamim Bernardo <juliobenjamimbernardo36@gmail.com>
2025-04-05 17:13:59 +0200

    new

--- mentorados/models.py
+++ mentorados/models.py
@@ -10,17 +10,21 @@ class Navigators(models.Model):
         return self.nome
 
 class Mentorados(models.Model):
-
     estagio_choices = (
-        ('E1', '10-100k'),
-        ('E2', '10-1kk')
+        ('I', 'Iniciante'),
+        ('M', 'Intermediário'),
+        ('A', 'Avançado'),
+    
     )
     nome = models.CharField( max_length=255)
     foto = models.ImageField(upload_to='fotos', null=True, blank=True)
     criado_em = models.DateField(auto_now_add=True)
     navigators = models.ForeignKey(Navigators, null=True, blank=True, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     estagios = models.CharField(max_length=2, choices=estagio_choices)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    estagio_choices = estagio_choices
 
 
     def __str__(self):
--- mentorados/views.py
+++ mentorados/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from. models import Mentorados, Navigators
+from django.contrib import messages
+from django.contrib.messages import constants
+
 
 def mentorados(request):
     if not request.user.is_authenticated:
@@ -8,3 +11,24 @@ def mentorados(request):
     if request.method == 'GET':
         navigators = Navigators.objects.filter(user=request.user)
         return render (request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        estagio = request.POST.get("estagio")
+        navigator = request.POST.get('navigator')
+
+        mentorado = Mentorados(
+            nome=nome,
+            foto=foto,
+            estagio=estagio,
+            navigator_id=navigator,
+            user=request.user
+        )
+
+        mentorado.save()
+
+        messages.add_message(request, constants. SUCCESS, 'Mentorados cadastrado com sucesso.')
+        return redirect('mentorados')
+
+   
+
